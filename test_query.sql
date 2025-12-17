52c6c6175b4c4f4d14d6c2698146bf639a69c94c
Mohit Kumar <mkumar@kaplaninc.com>
2025-12-17 14:18:33 +0530

    removed extraa sql files

--- test_query.sql
+++
@@ -1,5 +0,0 @@
-select distinct actf.activity_name,actf.activity_title 
-from kna.student_performance.activity_fact actf
-where 
-actf.activity_name  in ({act_name_placeholder})
-and actf.date_created >= '2024-01-01' 
